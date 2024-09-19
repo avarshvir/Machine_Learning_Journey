1216c37bd6e02eea085220e3b4e8184983c22219
Arshvir <avarshvir@gmail.com>
2024-09-19 15:22:50 +0530

    commit

---
+++ 1_python/1_11_open_cv/open_cv.py
@@ -0,0 +1,3 @@
+print("I already have a dedicated computer vision library.")
+print("From the following link, you can check my computer vision library:")
+print("https://github.com/avarshvir/Computer_Vision")
